9a258d7fe00f93495d4c9a7eef889f1544b6ab18
sooyeon Shim <syshim77@sooyeonui-MacBook-Pro.local>
2018-01-14 00:17:43 +0900

    complete hitter segmentedControl button

--- HomeBase/HomeBase/RegisterPlayerInfoViewController.swift
+++ HomeBase/HomeBase/RegisterPlayerInfoViewController.swift
@@ -13,12 +13,15 @@ class RegisterPlayerInfoViewController: UIViewController {
     @IBOutlet weak var position: UITextField!
     @IBOutlet weak var number: UITextField!
     @IBOutlet weak var pitcher: UISegmentedControl!
+    @IBOutlet weak var hitter: UISegmentedControl!
+    @IBOutlet weak var done: UIButton!
     
     let positionData = [String](arrayLiteral: "선발투수", "중간계투", "마무리투수", "포수", "1루수", "2루수", "3루수", "유격수", "좌익수", "중견수", "우익수", "지명타자")
     
     private var positionText: String = ""
     private var numberText: Int = 0
     private var pitcherText: String = ""
+    private var hitterText: String = ""
 
     // MARK: Action
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
@@ -37,6 +40,9 @@ class RegisterPlayerInfoViewController: UIViewController {
         
         let pitcherSegmentedControl = UISegmentedControl(items: ["좌", "우"])
         pitcherSegmentedControl.selectedSegmentIndex = 1
+        
+        let hitterSegmentedControl = UISegmentedControl(items: ["좌", "우"])
+        hitterSegmentedControl.selectedSegmentIndex = 1
     }
     
     // MARK: Function
