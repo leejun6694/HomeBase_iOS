82117d209184e8d5bf2521484411de163b1a506c
JUNLEE <leejun6694@gmail.com>
2018-02-06 19:51:47 +0900

    complete team logo

--- HomeBase/HomeBase/MainTeamInfoView.swift
+++ HomeBase/HomeBase/MainTeamInfoView.swift
@@ -12,50 +12,95 @@ class MainTeamInfoView: UIView {
     
     // MARK: Properties
     
-    lazy var teamImageView:UIImageView =  {
-        let teamImageView = UIImageView()
-        teamImageView.image = #imageLiteral(resourceName: "backgroundMain")
-        teamImageView.translatesAutoresizingMaskIntoConstraints = false
+    private lazy var teamPhotoImageView:UIImageView =  {
+        let teamPhotoImageView = UIImageView(image: #imageLiteral(resourceName: "backgroundMain"))
+        teamPhotoImageView.translatesAutoresizingMaskIntoConstraints = false
         
-        return teamImageView
+        return teamPhotoImageView
     }()
     
-    lazy var contentView:UIView = {
+    private lazy var contentView:UIView = {
         let contentView = UIView()
         contentView.backgroundColor = .white
         contentView.translatesAutoresizingMaskIntoConstraints = false
         
         return contentView
     }()
+    
+    private lazy var teamLogoImageView:UIImageView = {
+        let teamLogoImageView = UIImageView(image: #imageLiteral(resourceName: "team_logo"))
+        teamLogoImageView.layer.cornerRadius = teamLogoImageView.frame.size.width / 2
+        teamLogoImageView.layer.borderColor = UIColor.black.withAlphaComponent(0.15).cgColor
+        teamLogoImageView.layer.borderWidth = 1.0
+        teamLogoImageView.clipsToBounds = true
+        teamLogoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return teamLogoImageView
+    }()
+    
+    // MARK: Draw
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        self.addSubview(teamImageView)
-        self.addConstraints(teamImageViewConstraints())
+        self.addSubview(teamPhotoImageView)
+        self.addConstraints(teamPhotoImageViewConstraints())
         self.addSubview(contentView)
         self.addConstraints(contentViewConstraints())
+        self.addSubview(teamLogoImageView)
+        self.addConstraints(teamLogoImageViewConstraints())
     }
 }
 
 extension MainTeamInfoView {
-    func teamImageViewConstraints() -> [NSLayoutConstraint] {
+    private func teamPhotoImageViewConstraints() -> [NSLayoutConstraint] {
         let topConstraint = NSLayoutConstraint(
-            item: teamImageView, attribute: .top, relatedBy: .equal,
+            item: teamPhotoImageView, attribute: .top, relatedBy: .equal,
             toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0)
-        let leadingConstraint = NSLayoutConstraint(item: teamImageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0)
-        let trailingConstraint = NSLayoutConstraint(item: teamImageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0)
-        let heightConstraint = NSLayoutConstraint(item: teamImageView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 280/636, constant: 0.0)
+        let leadingConstraint = NSLayoutConstraint(
+            item: teamPhotoImageView, attribute: .leading, relatedBy: .equal,
+            toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0)
+        let trailingConstraint = NSLayoutConstraint(
+            item: teamPhotoImageView, attribute: .trailing, relatedBy: .equal,
+            toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+        let heightConstraint = NSLayoutConstraint(
+            item: teamPhotoImageView, attribute: .height, relatedBy: .equal,
+            toItem: teamPhotoImageView, attribute: .width, multiplier: 280/414, constant: 0.0)
         
         return [topConstraint, leadingConstraint, trailingConstraint, heightConstraint]
     }
     
-    func contentViewConstraints() -> [NSLayoutConstraint] {
-        let topConstraint = NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: teamImageView, attribute: .bottom, multiplier: 1.0, constant: 0.0)
-        let leadingConstraint = NSLayoutConstraint(item: contentView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0)
-        let trailingConstraint = NSLayoutConstraint(item: contentView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0)
-        let bottomConstraint = NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+    private func contentViewConstraints() -> [NSLayoutConstraint] {
+        let topConstraint = NSLayoutConstraint(
+            item: contentView, attribute: .top, relatedBy: .equal,
+            toItem: teamPhotoImageView, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        let leadingConstraint = NSLayoutConstraint(
+            item: contentView, attribute: .leading, relatedBy: .equal,
+            toItem: self, attribute: .leading, multiplier: 1.0, constant: 0.0)
+        let trailingConstraint = NSLayoutConstraint(
+            item: contentView, attribute: .trailing, relatedBy: .equal,
+            toItem: self, attribute: .trailing, multiplier: 1.0, constant: 0.0)
+        let bottomConstraint = NSLayoutConstraint(
+            item: contentView, attribute: .bottom, relatedBy: .equal,
+            toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0)
         
         return [topConstraint, leadingConstraint, trailingConstraint, bottomConstraint]
+    }
+    
+    private func teamLogoImageViewConstraints() -> [NSLayoutConstraint] {
+        let centerXConstraint = NSLayoutConstraint(
+            item: teamLogoImageView, attribute: .centerX, relatedBy: .equal,
+            toItem: teamPhotoImageView, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        let centerYConstraint = NSLayoutConstraint(
+            item: teamLogoImageView, attribute: .centerY, relatedBy: .equal,
+            toItem: teamPhotoImageView, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        let widthConstraint = NSLayoutConstraint(
+            item: teamLogoImageView, attribute: .width, relatedBy: .equal,
+            toItem: teamPhotoImageView, attribute: .width, multiplier: 130/414, constant: 1.0)
+        let heightConstraint = NSLayoutConstraint(
+            item: teamLogoImageView, attribute: .height, relatedBy: .equal,
+            toItem: teamLogoImageView, attribute: .width, multiplier: 1.0, constant: 0.0)
+        
+        return [centerXConstraint, centerYConstraint, widthConstraint, heightConstraint]
     }
 }
