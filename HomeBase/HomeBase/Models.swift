3dbe198555e459b911e671dda7d6929c1c463646
JUNLEE <leejun6694@gmail.com>
2018-02-09 18:52:16 +0900

    refactoring cloud functions

--- HomeBase/HomeBase/Models.swift
+++ HomeBase/HomeBase/Models.swift
@@ -7,7 +7,55 @@
 //
 
 import Foundation
+import UIKit
 
-struct Models {
+class HBUser {
+    var email:String
+    var name:String
+    var birth:String
+    var teamCode:String
+    var provider:String
+    
+    init(email:String, name:String, birth:String, teamCode:String, provider:String) {
+        self.email = email
+        self.name = name
+        self.birth = birth
+        self.teamCode = teamCode
+        self.provider = provider
+    }
+}
 
+class HBPlayer {
+    var name:String
+    var position:String
+    var backNumber:Int
+    var height:Int
+    var weight:Int
+    var batPosition:String
+    var pitchPosition:String
+    
+    init(name:String, position:String, backNumber:Int, height:Int, weight:Int, batPoition:String, pitchPosition:String) {
+        
+        self.name = name
+        self.position = position
+        self.backNumber = backNumber
+        self.height = height
+        self.weight = weight
+        self.batPosition = batPoition
+        self.pitchPosition = pitchPosition
+    }
+}
+
+class HBTeam {
+    var name:String
+    var logo:String
+    var description:String
+    var homeStadium:String
+    
+    init(name:String, logo:String, description:String, homeStadium:String) {
+        self.name = name
+        self.logo = logo
+        self.description = description
+        self.homeStadium = homeStadium
+    }
 }
