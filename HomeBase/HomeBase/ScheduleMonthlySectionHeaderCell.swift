317af4e4a1a3390459a1a205d533495dddbe1bf0
JUNLEE <leejun6694@gmail.com>
2018-04-03 02:53:48 +0900

    fixed schedule table view header view

---
+++ HomeBase/HomeBase/ScheduleMonthlySectionHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  ScheduleMonthlySectionHeaderCell.swift
+//  HomeBase
+//
+//  Created by JUN LEE on 2018. 4. 3..
+//  Copyright © 2018년 JUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleMonthlySectionHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
