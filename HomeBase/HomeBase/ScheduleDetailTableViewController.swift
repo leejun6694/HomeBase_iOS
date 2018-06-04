3c29255c4aae4927b83c0267f9652ece0acbadad
JUNLEE <leejun6694@gmail.com>
2018-06-04 15:54:33 +0900

    complete team members sorted by back number

--- HomeBase/HomeBase/ScheduleDetailTableViewController.swift
+++ HomeBase/HomeBase/ScheduleDetailTableViewController.swift
@@ -56,6 +56,8 @@ class ScheduleDetailTableViewController: UITableViewController {
                         if let teamData = teamData {
                             self.teamData = teamData
                             self.playerList = teamData.members
+                            self.playerList =
+                                self.playerList.sorted(by: { $0.backNumber < $1.backNumber })
                             
                             self.tableView.reloadData()
                             self.viewEnabled(self.view)
@@ -170,6 +172,8 @@ class ScheduleDetailTableViewController: UITableViewController {
         
         setupRefreshControl()
         self.playerList = teamData.members
+        self.playerList =
+            self.playerList.sorted(by: { $0.backNumber < $1.backNumber })
         
         scheduleDetailInfoView.homeTeamButton.addTarget(
             self,
